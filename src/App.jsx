38040dff2e80ae6ec11be6bbd7d5e7ef915c62ad
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-20 21:35:46 +0600

    project setup and font added done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Featured from './components/Featured'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Featured></Featured>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Featured.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../App.css'
+
+const Featured = () => {
+    return (
+        <div className='featured'>
+            <h1 className='featured-title'>Featured <span className='primary'>projects</span></h1>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
